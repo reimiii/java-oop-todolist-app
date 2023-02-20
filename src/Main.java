9af3494f52dc5837f36a8d8d6c093a9f5918d28a
z02 <miigo@duck.com>
2023-02-20 22:07:23 +0700

    Menampikan todo list, test menampilkan todo list

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,28 @@ public class Main {
     public static String[] model = new String[10];
 
     public static void main(String[] args) {
-
+        testShowTodoList();
     }
 
     // show todo list
     public static void showTodoList() {
+        for (var i = 0; i < model.length; i++) {
+            var todo = model[i];
+            var no = i + 1;
 
+            if (todo != null) {
+                System.out.println(no + ". " + todo);
+            }
+        }
+    }
+
+    // test for show todo in list
+    public static void testShowTodoList() {
+        model[0] = "Java Dasar";
+        model[1] = "Java Dasar TodoList";
+        model[2] = "Java OOP";
+        model[3] = "Java Lanjutan";
+        showTodoList();
     }
 
     // add todo ke list
