1e4ae8c528bb8ac97251926d39824bbe704be0bd
z02 <miigo@duck.com>
2023-02-20 21:56:35 +0700

    model, menentukan business logic, menentukan view

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,38 @@
 public class Main {
 
+    public static String[] model = new String[10];
+
     public static void main(String[] args) {
+
+    }
+
+    // show todo list
+    public static void showTodoList() {
+
+    }
+
+    // add todo ke list
+    public static void addTodoList() {
+
+    }
+
+    // remove todo from list
+    public static void removeTodoList() {
+
+    }
+
+    // view todo list
+    public static void viewShowTodoList() {
+
+    }
+
+    // view for add todo list
+    public static void viewAddTodoList() {
+
+    }
+
+    // view for remove todo from list
+    public static void viewRemoveTodoList() {
 
     }
 }
