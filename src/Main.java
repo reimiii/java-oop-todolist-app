1937f20e6ac6bb4e2c67ee481cd678c93d902f29
z02 <miigo@duck.com>
2023-02-21 00:00:33 +0700

    remove todo and test remove todo

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
     public static String[] model = new String[10];
 
     public static void main(String[] args) {
-        testAddTodoList();
+        System.out.println(model.length);
+        testRemoveTodoList();
     }
 
     // show todo list
@@ -15,6 +16,7 @@ public class Main {
             if (todo != null) {
                 System.out.println(no + ". " + todo);
             }
+
         }
     }
 
@@ -31,7 +33,7 @@ public class Main {
     public static void addTodoList(String todo) {
         // check data full?
         var isFull = true;
-        for (var i = 0; i < model.length; i++){
+        for (var i = 0; i < model.length; i++) {
             if (model[i] == null) {
                 // model still not full?
                 isFull = false;
@@ -40,7 +42,7 @@ public class Main {
         }
 
         // if full is true? then index length array resize 2*
-        if (isFull){
+        if (isFull) {
             var temp = model;
             model = new String[model.length * 2];
 
@@ -59,8 +61,8 @@ public class Main {
     }
 
     // test for add todo to list
-    public static void testAddTodoList(){
-        for (var i = 0; i < 25; i++){
+    public static void testAddTodoList() {
+        for (var i = 0; i < 25; i++) {
             addTodoList("test tode ke. " + i);
         }
 
@@ -68,8 +70,49 @@ public class Main {
     }
 
     // remove todo from list
-    public static void removeTodoList() {
+    public static boolean removeTodoList(Integer number) {
+        if ((number - 1) >= model.length) {
+            return false;
+        } else if (model[number - 1] == null) {
+            return false;
+        } else {
+            for (var i = (number - 1); i < model.length; i++) {
+                // hmmm misal panjang nya 10 - 1 = 9, maka index ke 9 = null
+                // jika i itu 10 - 1 = 9, panjang array 10 - 1 = 9 maka
+                // model[9] = null, okay make sense, ngerti ngerti!
+                if (i == (model.length - 1)) {
+                    model[i] = null;
+                } else {
+                    // else nya itu maka si model[8] value nya = model[8 + 1]
+                    model[i] = model[i + 1];
+                }
+            }
+            return true;
+        }
+    }
 
+    // test remove todo from list
+    public static void testRemoveTodoList() {
+        addTodoList("one");
+        addTodoList("two");
+        addTodoList("three");
+        addTodoList("four");
+        addTodoList("five");
+
+        System.out.println("-------------------" + model.length);
+        showTodoList();
+        System.out.println("-------------------");
+
+        var response = removeTodoList(20);
+        System.out.println(response);
+
+        response = removeTodoList(9);
+        System.out.println(response);
+
+        response = removeTodoList(2);
+        System.out.println(response);
+
+        showTodoList();
     }
 
     // view todo list
