709526189c9c049cba5b909b96b2ab94c9ba10ca
reimiii <miigo@duck.com>
2023-03-13 21:15:52 +0700

    Membuat-Repository

---
+++ src/repository/TodoListRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import entity.TodoList;
+
+public interface TodoListRepository {
+    TodoList[] getAll();
+
+    void add(TodoList todoList);
+
+    void remove(Integer number);
+}
