2fdbd7661b67d0cdc5159039ef9f14548814c38f
reimiii <miigo@duck.com>
2023-03-14 19:27:03 +0700

    Create a service and Repository-and-Service-View-TodoList

---
+++ src/service/TodoListServiceImpl.java
@@ -0,0 +1,36 @@
+package service;
+
+import entity.TodoList;
+import repository.TodoListRepository;
+
+public class TodoListServiceImpl implements TodoListService {
+    private TodoListRepository todoListRepository;
+
+    public TodoListServiceImpl(TodoListRepository todoListRepository) {
+        this.todoListRepository = todoListRepository;
+    }
+
+    @Override
+    public void showTodoList() {
+        TodoList[] model = todoListRepository.getAll();
+        System.out.println("TODO-LIST");
+        for (var i = 0; i < model.length; i++) {
+            var todo = model[i];
+            var no = i + 1;
+
+            if (todo != null) {
+                System.out.println(no + ". " + todo);
+            }
+        }
+    }
+
+    @Override
+    public void addTodoList(String todo) {
+
+    }
+
+    @Override
+    public void removeTodoList(Integer number) {
+
+    }
+}
