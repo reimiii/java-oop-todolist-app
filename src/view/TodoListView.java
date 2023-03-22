4e14fa80af352e3fc6cd31c1b41e5fe5cab260a4
reimiii <miigo@duck.com>
2023-03-22 11:23:20 +0700

    Implementasikan View Todo List

--- src/view/TodoListView.java
+++ src/view/TodoListView.java
@@ -1,8 +1,36 @@
 package view;
 
-public class TodoListView {
-    public void showTodoList() {
+import service.TodoListService;
+import util.InputUtil;
 
+public class TodoListView {
+    private TodoListService service;
+
+    public TodoListView(TodoListService service) {
+        this.service = service;
+    }
+
+    public void showTodoList() {
+        while (true) {
+            service.showTodoList();
+
+            System.out.println("Menu");
+            System.out.println("1. Tambah");
+            System.out.println("2. Hapus");
+            System.out.println("x. Keluar");
+
+            var input = InputUtil.input("Pilih");
+
+            if (input.equals("1")) {
+                addTodoList();
+            } else if (input.equals("2")) {
+                removeTodoList();
+            } else if (input.equals("x")) {
+                break;
+            } else {
+                System.out.println("Not valid input");
+            }
+        }
     }
 
     public void addTodoList() {
